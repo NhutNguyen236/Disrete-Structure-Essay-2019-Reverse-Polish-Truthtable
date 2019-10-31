d270ccf256f3279a96cd5ba57c6c1e1b4583e8cb
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-10-31 23:09:59 +0700

    Update truthtable_demo.py

--- truthtable_demo.py
+++ truthtable_demo.py
@@ -1,11 +1,18 @@
 import sys
-
+from collections import OrderedDict
 #Impliccation definition
 def implies(lo1,lo2):
     return (not lo1) or lo2
 
-x = 'P~QRP&>|R>'
+x = 'RPQ&|'
 y = x
+numb_of_opt = []
+for i in x:
+    if not i.isalpha():
+        numb_of_opt.append(i)
+print(numb_of_opt)
+numopt = len(numb_of_opt)
+print(numopt)
 vars = []
 for i in x:
     print(i,end=' ')
@@ -14,25 +21,30 @@ for i in x:
 #new_vars just contains only operrands
 new_vars = sorted(vars)
 new_vars_y = new_vars
+math1 = []
+math = []
+
 #For operrand separate
 for i in range(2**len(new_vars)):
     print('')
-    string = bin(i)[2:].rjust(len(new_vars),'0')
+    string = bin(i)[2:].zfill(len(new_vars))
     bools = list(map((lambda x: True if x=='1' else False), string))
     a = dict(zip(new_vars,bools))
-    math1 = []
-    for j in x:
+    print(a)
+    for j in  "".join(OrderedDict.fromkeys(x)):
         if j.isalpha():
             math1.append(a[j])
-            print(int(a[j]),end=' ')
+            
 print('\n','====================')
 #For operator group up
+math2 = []
+math3 = []
 for i in range(2**len(new_vars_y)):
   print('')
   string = bin(i)[2:].rjust(len(new_vars_y),'0')
   bools = list(map((lambda x: True if x=='1' else False), string))
   a = dict(zip(new_vars_y,bools))
-  math2 = []
+  math.append(a)
   for j in y:
         if j.isalpha():
             math2.append(a[j])
@@ -40,25 +52,35 @@ for i in range(2**len(new_vars_y)):
         elif j == '&':
             math2[-2] = math2[-1] & math2[-2]
             print(int(math2[-2]),end=' ')
+            math3.append(math2[-2])
             del math2[-1]
         elif j == '|':
             math2[-2] = math2[-1] | math2[-2]
             print(int(math2[-2]),end=' ')
+            math3.append(math2[-2])
             del math2[-1]
         elif j == '=':
             math2[-2] = implies(math2[-2],math2[-1]) and implies(math2[-1],math2[-2])
             print(int(math2[-2]),end=' ')
+            math3.append(math2[-2])
             del math2[-1]
         elif j == '>':
             math2[-2] = implies(math2[-2],math2[-1])
             print(int(math2[-2]),end=' ')
+            math3.append(math2[-2])
             del math2[-1]
         elif j == '<':
             math2[-2] = implies(math2[-1],math2[-2])
             print(int(math2[-2]),end=' ')
-            del math2[-1] 
+            math3.append(math2[-2])
+            del math2[-1]
         elif j == '~':
             math2[-1] = not math2[-1]
             print(int(math2[-1]),end=' ')
+            math3.append(math2[-1])
         else:
             print("Error: Invalid Character")
+            
+y = zip(*[iter(math3)]*numopt)
+z = list(y)
+print(z)
