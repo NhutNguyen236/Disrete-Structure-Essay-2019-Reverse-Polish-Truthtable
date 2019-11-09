9f9c45c6111c80baad5a227c3fddc420a4018b77
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-11-09 23:13:13 +0700

    Update Essay.py

--- Essay.py
+++ Essay.py
@@ -42,30 +42,44 @@ def Infix2Postfix(Infix):
     tokens = Infix.split()
     Postfix=[]
     stack = Stack()
+    #access to every token in tokens 
     for token in tokens:
+        #if the checking token is alphabetic, put it in Postfix
         if token.isalpha():
             Postfix.append(token)
+        #if not but token is a left parenthese push it to stack 
         elif token == '(':
             stack.push(token)
+        #if it is not but it is a right parenthese, then check:
         elif token == ')':
             while True:
+                #using a vari named temp to keep stack pop
                 temp = stack.pop()
+                #if temp is empty or it is left parenthses then break go to next step
                 if temp is None or temp == '(':
                     break
+                #if not but thing inside temp is alphabetic put it in Postfix
                 elif not temp.isalpha():
                     Postfix.append(temp)
+        #if it is not right parenthese but it is a '~' and followed by another '~', push both to the satck
         elif token == '~' and stack.peek() == '~':
                 stack.push(token)
+        #if none of the above 
         else:
+            #when stack is not empty then peek each element then compare precedence if peeked token has higher prec than the token is 
+            #pushed to stack then pop it out and put in Postfix and repeat
             if not stack.empty():
                 temp = stack.peek()
                 while not stack.empty() and precedence[temp] >= precedence[token]:
                     Postfix.append(stack.pop())
                     temp = stack.peek()
             stack.push(token)
+    #this step is reached to the end of postfix and stack is not empty yet then pop all out and put the rest in postfix
     while not stack.empty():
         Postfix.append(stack.pop())
+    #Rejoin the list named Postfix to turn it into a string
     Postfix = ''.join(Postfix)
+    #return result 
     return Postfix
 
 # Define an implication function 
@@ -73,7 +87,7 @@ def implies(lo1,lo2):
     return (not lo1) or lo2
 
 def Postfix2Truthtable(Postfix):
-    #Count number of operrands
+    #Creating a list to contain only operands named opd
     opd = []
     for i in Postfix:
         if i.isalpha() and not i in opd:
@@ -86,7 +100,9 @@ def Postfix2Truthtable(Postfix):
     #creat an empty sub_truthtable :)
     sub = []
     for i in range(rows):
+        #Turning each i into binary form and fill up with right amount of 0 
         string = bin(i)[2:].zfill(len(order_opd))
+        #turn 1 into True and 0 into False and map them up as a list ready for putting them as rows
         bools = list(map((lambda x: True if x=='1' else False),string))
         sub.append(bools)
     #turn sub_truthtable into tuple ready for mounting
@@ -97,10 +113,13 @@ def Postfix2Truthtable(Postfix):
     for i in range(rows):
         string = bin(i)[2:].zfill(len(order_opd))
         bools = list(map((lambda x: True if x=='1' else False),string))
-        a = dict(zip(order_opd,bools))
+        el = dict(zip(order_opd,bools))
+        #loop in every token of Postfix
         for j in Postfix:
+            #check if j is alphabet, if yes append dictionary of i to temporary list name math2
             if j.isalpha():
-                math2.append(a[j])
+                math2.append(el[j])
+            # if it is &,|,=,> calculate 2 first operand before that operator then delete itself to fit in math3
             elif j =='&':
                 math2[-2] = math2[-1] and math2[-2]
                 math3.append(math2[-2])
@@ -120,6 +139,7 @@ def Postfix2Truthtable(Postfix):
             elif j == '~':
                 math2[-1] = not math2[-1]
                 math3.append(math2[-1])
+            #if there is something diffrent from ~&|>= then append Invalid to math3 
             else:
                 math3.append("Invalid")
     #create an empty truthtable waiting for tuple up :)
